573fcd53bdd69c71f044d0f31dc872e614de5d57
Heiner Landero <heinerlandero@gmail.com>
2024-02-13 21:33:12 -0500

    Create app react

---
+++ library_files/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { TestComponent } from './components/TestComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TestComponent/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ library_files/src/components/TestComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const TestComponent = () => {
+  return (
+    <div>
+        <h1>
+            This Beging
+        </h1>
+    </div>
+  )
+}
