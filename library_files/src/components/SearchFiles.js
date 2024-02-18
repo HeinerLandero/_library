ea42681529e3074ecea40e5983c2b22df27ccb45
Heiner Landero <heinerlandero@gmail.com>
2024-02-17 23:35:24 -0500

    added logic add movies

--- library_files/src/components/SearchFiles.js
+++ library_files/src/components/SearchFiles.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const SearchFiles = () => {
     const title = "Search Engine";
     const text_btn = "Search"; 
-    const getDataForm = e =>{
+    const getNameMovie = e =>{
         e.preventDefault();
         alert('esto es una prueba')
     };
@@ -11,11 +11,11 @@ export const SearchFiles = () => {
     <>
         <div className="search">
             <h3 className="title">{title}</h3>
-            <form onSubmit={getDataForm}>
+            <form onSubmit={getNameMovie}>
                 <input  type="text"
-                        name="title"
-                        id="title"
-                        placeholder="title" />
+                        name="title_search"
+                        id="title_search"
+                        placeholder="Title search" />
                 <button className='btn-search' id="search">{text_btn}</button>
             </form>
         </div>
--- library_files/src/components/UploaderFiles.js
+++ library_files/src/components/UploaderFiles.js
@@ -1,14 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const UploaderFiles = () => {
+  const titleComponent = 'Add Movie';
+  const [movieState, setMovieState] = useState({
+    title: "",
+    description:""
+  });
+  const {title,description} = movieState;
+  const getDataForm = e =>{
+    e.preventDefault();
+    let target = e.target;
+    let title = target.title.value;
+    let description = target.description.value;
+    console.table( {title,description});
+
+    let movie = {
+      id: new Date().getTime(),
+      title,
+      description
+    };
+    setMovieState(movie);
+    saveStorage(movie);
+    console.log(movieState);
+  }
+    const saveStorage = movie =>{
+      let elements = JSON.parse(localStorage.getItem('movies'));
+        if(Array.isArray(elements)){
+          elements.push(movie);
+        }else{
+          elements = [movie];
+        }
+        localStorage.setItem('movies', JSON.stringify(elements));
+        return movie;
+  }
   return (
     <>
         <div className="add">
-                <h3 className="title">Añadir pelicula</h3>
-                <form>
-                    <input type="text" id="title" placeholder="Titulo" />
-                    <textarea id="description" placeholder="Descripción"></textarea>
-                    <input type="submit" id="save" value="Guardar" />
+                <h3 className="title">{titleComponent}</h3>
+                <strong>
+                  {(title && description) && "Has añadido la pelicula " + title + " con exito" }
+                </strong>
+                <form onSubmit={getDataForm}>
+                    <input 
+                      type="text"
+                      id="title"
+                      name="title"
+                      placeholder="Title"/>
+                    <textarea id="description" placeholder="Description"></textarea>
+                    <input  className='btn_upload' type="submit" id="save" value="Save" />
                 </form>
         </div>
     </>
